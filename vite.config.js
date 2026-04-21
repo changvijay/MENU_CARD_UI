a5ed79319f4d96056aaecf851897e034e21d9900
chang <changvijay54@gmail.com>
2026-04-22 00:19:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://menu-card-api-yvzycdnaqq-el.a.run.app',
-//         changeOrigin: true,
-//         secure: false,
-//         ws: true,
-//       },
-//       '/auth/login': {
-//         target: 'https://menu-card-api-yvzycdnaqq-el.a.run.app',
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// })
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://menu-card-api-yvzycdnaqq-el.a.run.app',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      '/auth/login': {
+        target: 'https://menu-card-api-yvzycdnaqq-el.a.run.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
